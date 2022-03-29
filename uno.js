4b0b9876d84256a5d59f40bbff22cbc32dccf5f3
unknown <tipito4085@gmail.com>
2022-03-29 17:27:16 -0500

    Final (ojalá)

--- uno.js
+++ uno.js
@@ -8,6 +8,29 @@ function escuchar(event) {
     let name = document.getElementById("name").value
     let id = document.getElementById("id").value
     let email = document.getElementById("email").value
-    console.log(empanada + ", " +quantity +", " + name+", "+id+", "+email)
+    console.log(empanada + ", " + quantity + ", " + name + ", " + id + ", " + email)
+
+    let carne = 3000
+    let pollo = 3200
+    let papa = 3200
+    let paisa = 3500
+    let ranchera = 3500
+
+    if (empanada == "carne") {
+        let ressult = quantity * carne
+        alert("Su total es de: " + ressult)
+    }
+    else if (empanada == "paisa") {
+        let ressult = quantity * paisa
+        alert("Su total es de: " + ressult)
+    }
+    else if (empanada == "ranchera") {
+        let ressult = quantity * ranchera
+        alert("Su total es de: " + ressult)
+    }
+    else if (empanada == "pollo" || "papa") {
+        let ressult = quantity * pollo
+        alert("Su total es de: " + ressult)
+    }
 }
 
